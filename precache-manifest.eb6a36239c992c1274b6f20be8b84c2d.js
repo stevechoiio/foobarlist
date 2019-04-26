9b62371c8a8232d59d2138f9bb0c95808c0dd732
Steve Choi <stevechoi93@gmail.com>
2019-04-26 13:49:18 -0700

    Updates

---
+++ precache-manifest.eb6a36239c992c1274b6f20be8b84c2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd7fe17b7a7f40e097e8",
+    "url": "/foobarlist/static/css/main.9385366b.chunk.css"
+  },
+  {
+    "revision": "bd7fe17b7a7f40e097e8",
+    "url": "/foobarlist/static/js/main.df1fde06.chunk.js"
+  },
+  {
+    "revision": "c5e294d27fb06074ef0e",
+    "url": "/foobarlist/static/js/runtime~main.65510125.js"
+  },
+  {
+    "revision": "8d5c724a2e659e2a9eb9",
+    "url": "/foobarlist/static/js/2.2f102ba0.chunk.js"
+  },
+  {
+    "revision": "7ba1c7dcd0b7db51d160c16e3b492176",
+    "url": "/foobarlist/index.html"
+  }
+];
